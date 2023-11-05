754891d9cf08093d0722bb84686eb00fca1e34cb
wioenena-q <wioenena@gmail.com>
2023-11-06 01:33:51 +0300

    test: cws_str_len tested

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include "include/cws.h"
+#include <assert.h>
+#include <string.h>
+
+void test_cws_str_len();
 
 int main(void) {
-  unsigned long len = cws_str_len("Hello World");
-  printf("%lu\n", len);
+  test_cws_str_len();
+}
+
+void test_cws_str_len() {
+  const char* str= "Hello World!";
+  unsigned long len = cws_str_len(str);
+  assert(len == strlen(str));
 }
