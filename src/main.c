95bbfa8cab0b20b737acd5e67d47cee10dfb922e
wioenena-q <wioenena@gmail.com>
2023-11-06 02:54:05 +0300

    test: cws_char_index_of tested

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 void test_cws_str_len();
+void test_cws_char_index_of();
 
 int main(void) {
   test_cws_str_len();
@@ -13,4 +14,10 @@ void test_cws_str_len() {
   const char* str= "Hello World!";
   unsigned long len = cws_str_len(str);
   assert(len == strlen(str));
+}
+
+void test_cws_char_index_of() {
+  const char* str = "Hello World!";
+  long index = cws_char_index_of(str, 'o');
+  assert(index == 4);
 }
