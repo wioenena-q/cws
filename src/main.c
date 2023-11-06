2bccafab89f201ad5e03ec520aabeea570b09c58
wioenena-q <wioenena@gmail.com>
2023-11-06 03:28:42 +0300

    test: all tests passed

--- src/main.c
+++ src/main.c
@@ -5,19 +5,35 @@
 
 void test_cws_str_len();
 void test_cws_char_index_of();
+void test_cws_index_of();
 
 int main(void) {
   test_cws_str_len();
+  test_cws_char_index_of();
+  test_cws_index_of();
 }
 
 void test_cws_str_len() {
   const char* str= "Hello World!";
   unsigned long len = cws_str_len(str);
   assert(len == strlen(str));
+  assert(cws_str_len(NULL) == 0);
 }
 
 void test_cws_char_index_of() {
   const char* str = "Hello World!";
-  long index = cws_char_index_of(str, 'o');
-  assert(index == 4);
+  assert(cws_char_index_of(str, 'o') == 4);
+  assert(cws_char_index_of(str, 'H') == 0);
+  assert(cws_char_index_of(str, 'd') == 10);
+  assert(cws_char_index_of(str, '!') == 11);
+  assert(cws_char_index_of(NULL, '!') == -1);
+}
+
+void test_cws_index_of() {
+  const char* str = "Hello World!";
+  assert(cws_index_of(str, "Hello") == 0);
+  assert(cws_index_of(str, "llo") == 2);
+  assert(cws_index_of(str, "World!") == 6);
+  assert(cws_index_of(str, "???") == -1);
+  assert(cws_index_of(NULL, "World!") == -1);
 }
